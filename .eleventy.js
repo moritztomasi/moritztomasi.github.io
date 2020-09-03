228e60d53b1d04928ce48a9cb5e49ce1af6f6ae8
Moritz Tomasi <mt@findheim.at>
2020-09-03 09:35:29 +0200

    added docs folder

--- .eleventy.js
+++ .eleventy.js
@@ -47,7 +47,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "dist",
+      output: "docs",
       includes: "includes",
       layouts: "layouts",
       data: "data",
