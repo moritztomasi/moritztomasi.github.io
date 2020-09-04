ad8a7d05d1c6e0d271dd451d07d9edc29b32e2bb
Moritz Tomasi <mt@findheim.at>
2020-09-04 14:39:14 +0200

    added prettier config

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
-const htmlmin = require("html-minifier");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const moment = require("moment-timezone");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
-const pluginTOC = require("eleventy-plugin-toc");
+const htmlmin = require('html-minifier');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const moment = require('moment-timezone');
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const pluginTOC = require('eleventy-plugin-toc');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginNavigation);
@@ -14,39 +14,35 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
 
   eleventyConfig.addPassthroughCopy({
-    "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+    './node_modules/alpinejs/dist/alpine.js': './js/alpine.js',
   });
 
-  eleventyConfig.addPassthroughCopy({ "./src/assets": "./assets" });
+  eleventyConfig.addPassthroughCopy({ './src/assets': './assets' });
 
-  eleventyConfig.addShortcode("version", function () {
+  eleventyConfig.addShortcode('version', function () {
     return String(Date.now());
   });
 
-  eleventyConfig.addFilter("dateformat", function (dateIn) {
-    return moment(dateIn).tz("GMT").format("DD-MM-YYYY");
+  eleventyConfig.addFilter('dateformat', function (dateIn) {
+    return moment(dateIn).tz('GMT').format('DD-MM-YYYY');
   });
 
-  eleventyConfig.addFilter("sitemapdateformat", function (dateIn) {
-    return moment(dateIn).tz("GMT").format("YYYY-MM-DD");
+  eleventyConfig.addFilter('sitemapdateformat', function (dateIn) {
+    return moment(dateIn).tz('GMT').format('YYYY-MM-DD');
   });
 
   eleventyConfig.setLibrary(
-    "md",
+    'md',
     markdownIt({
       html: true,
       breaks: true,
       linkify: true,
       typographer: true,
-    }).use(markdownItAnchor)
+    }).use(markdownItAnchor),
   );
 
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    if (
-      process.env.ELEVENTY_PRODUCTION &&
-      outputPath &&
-      outputPath.endsWith(".html")
-    ) {
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (process.env.ELEVENTY_PRODUCTION && outputPath && outputPath.endsWith('.html')) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
@@ -60,11 +56,11 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: "src",
-      output: "docs",
-      includes: "includes",
-      layouts: "layouts",
-      data: "data",
+      input: 'src',
+      output: 'docs',
+      includes: 'includes',
+      layouts: 'layouts',
+      data: 'data',
     },
   };
 };
