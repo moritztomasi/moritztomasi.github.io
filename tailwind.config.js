541a788fe9107dc4d45ce4776870071510020a88
Moritz Tomasi <mt@findheim.at>
2020-09-26 11:04:24 +0200

    redesigned header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   purge: ["./**/*.js", "./**/*.html", "./**/*.njk"],
   theme: {
+    screens: {
+      xs: '380px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1200px',
+    },
     extend: {
       colors: {
         cyan: "#000000",
